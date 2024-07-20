9fd5ca2de8f76b2bc62dcb8cf040319bd7441471
Kevin Brito <06kevin29@gmail.com>
2024-07-20 12:30:42 -0400

    new config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_AWo9q1G06',
+        userPoolClientId: '5td57t5ajf7ok90hvord1m5pi6',
+        region: 'us-east-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
